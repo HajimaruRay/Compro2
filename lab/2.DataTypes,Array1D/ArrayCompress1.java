9ab39b5b3e21a0db51eb01f7bbb9ea4e3250c732
HajimaruRay <144349636+HajimaruRay@users.noreply.github.com>
2023-12-18 09:48:28 +0700

    Update ArrayCompress1.java

--- lab/2.DataTypes,Array1D/ArrayCompress1.java
+++ lab/2.DataTypes,Array1D/ArrayCompress1.java
@@ -5,6 +5,35 @@ public class ArrayCompress1
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+        int Howlong = sc.nextInt();
+        int number[] = new int[Howlong];
+        int numbersum[] = new int[Howlong];
+        int loop = 0,count = 0;
+
+        for (int i = 0;i < Howlong;i++)
+        {
+            number[i] = sc.nextInt();
+        }
+        int numbersplit = sc.nextInt();
+
+        for (int i = 0;i < Howlong;i++)
+        {
+            if (number[i] != numbersplit)
+            {
+                numbersum[loop] += number[i];
+            }
+            else if (number[i] == numbersplit)
+            {
+                loop++;
+                numbersum[loop] = numbersplit;
+                count++;
+                loop++;
+            }
+        }
+        for (int i = 0;i < (count + (count + 1));i++)
+        {
+            System.out.printf("%d ",numbersum[i]);
+        }
         sc.close();
     }    
 }
