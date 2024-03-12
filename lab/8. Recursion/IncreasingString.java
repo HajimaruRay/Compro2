8461f7f42ee15bca2b779626bf27df551eaea75d
HajimaruRay <144349636+HajimaruRay@users.noreply.github.com>
2024-03-12 14:29:24 +0700

    IncreasingString

---
+++ lab/8. Recursion/IncreasingString.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class IncreasingString 
+{
+    String makeIncreasingString(String str, char lastPrint) 
+    {
+        if (str.length() <= 0)
+        {
+            return str;
+        }
+
+        if (str.charAt(0) == 'A')
+        {
+            lastPrint = 'A';
+        }
+
+        if (str.charAt(0) > lastPrint || lastPrint == 'A')
+        {
+            lastPrint = str.charAt(0);
+            return str.charAt(0) + makeIncreasingString(str.substring(1), lastPrint);
+        }
+        else
+        {
+            return makeIncreasingString(str.substring(1), lastPrint);
+        }
+    }
+
+    public static void main(String[] args) 
+    {
+        IncreasingString inc = new IncreasingString();
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        String result = inc.makeIncreasingString(str, 'A');
+        System.out.print(result);
+        scan.close();
+    }
+}
